2e98ba971164ce5881a4654c76180a2d08292da9
Lucas Vallejo <97030519+lucasvallejodev@users.noreply.github.com>
2025-04-14 21:50:28 +0200

    feat: add simple wizard page

---
+++ web/components/CurrencyComboBox.tsx
@@ -0,0 +1,111 @@
+"use client"
+
+import * as React from "react"
+
+import { useMediaQuery } from "@/hooks/use-media-query"
+import { Button } from "@/components/ui/button"
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command"
+import {
+  Drawer,
+  DrawerContent,
+  DrawerTrigger,
+} from "@/components/ui/drawer"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+
+type Currency = {
+  value: string
+  label: string
+}
+
+const currencies: Currency[] = [
+  {
+    value: "EUR",
+    label: "Euro",
+  },
+  {
+    value: "USD",
+    label: "US Dollar",
+  }
+]
+
+export function CurrencyComboBox() {
+  const [open, setOpen] = React.useState(false)
+  const isDesktop = useMediaQuery("(min-width: 768px)")
+  const [selectedCurrency, setSelectedCurrency] = React.useState<Currency | null>(
+    null
+  )
+
+  if (isDesktop) {
+    return (
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button variant="outline" className="w-full justify-start">
+            {selectedCurrency ? <>{selectedCurrency.label}</> : <>+ Set currency</>}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-[200px] p-0" align="start">
+          <StatusList setOpen={setOpen} setSelectedCurrency={setSelectedCurrency} />
+        </PopoverContent>
+      </Popover>
+    )
+  }
+
+  return (
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerTrigger asChild>
+        <Button variant="outline" className="w-full justify-start">
+          {selectedCurrency ? <>{selectedCurrency.label}</> : <>+ Set currency</>}
+        </Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <div className="mt-4 border-t">
+          <StatusList setOpen={setOpen} setSelectedCurrency={setSelectedCurrency} />
+        </div>
+      </DrawerContent>
+    </Drawer>
+  )
+}
+
+function StatusList({
+  setOpen,
+  setSelectedCurrency,
+}: {
+  setOpen: (open: boolean) => void
+  setSelectedCurrency: (currency: Currency | null) => void
+}) {
+  return (
+    <Command>
+      <CommandInput placeholder="Filter currency..." />
+      <CommandList>
+        <CommandEmpty>No results found.</CommandEmpty>
+        <CommandGroup>
+          {currencies.map((currency) => (
+            <CommandItem
+              key={currency.value}
+              value={currency.value}
+              onSelect={(value) => {
+                setSelectedCurrency(
+                  currencies.find((priority) => priority.value === value) || null
+                )
+                setOpen(false)
+              }}
+            >
+              {currency.label}
+            </CommandItem>
+          ))}
+        </CommandGroup>
+      </CommandList>
+    </Command>
+  )
+}
